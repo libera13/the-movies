e3b3a3ad116db2d72bf36cab9ba52ba198acc334
Emil <emillibera@gmail.com>
2021-02-13 16:33:58 +0100

    add vue-youtube-embed

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import VueYoutubeEmbed from "vue-youtube-embed";
 
+Vue.use(VueYoutubeEmbed);
 Vue.config.productionTip = false;
 
 new Vue({
