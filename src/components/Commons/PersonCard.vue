d57e7a15df5e79615e1959377976d4353afa774c
Emil <emillibera@gmail.com>
2021-02-13 15:25:21 +0100

    added person card

---
+++ src/components/Commons/PersonCard.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="person-div" :style="{ minHeight: `${personDivSize}px` }">
+    <v-avatar :size="avatarSize" color="grey darken-4">
+      <v-img
+        v-if="person.profile_path"
+        :src="`https://image.tmdb.org/t/p/w185${person.profile_path}`"
+      >
+        <template v-slot:placeholder>
+          <v-row class="fill-height ma-0" align="center" justify="center">
+            <v-progress-circular
+              indeterminate
+              color="grey darken-2"
+            ></v-progress-circular>
+          </v-row>
+        </template>
+      </v-img>
+      <v-icon v-else size="92" color="grey darken-2">mdi-account</v-icon>
+    </v-avatar>
+    <div class="person-name">{{ person.name }}</div>
+    <div v-if="person.character" class="person-character">
+      as {{ person.character }}
+    </div>
+    <div v-if="person.job" class="person-character">{{ person.job }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PersonCard",
+  props: {
+    person: {
+      type: Object,
+      required: true
+    },
+    subheading: {
+      type: Boolean,
+      required: true
+    }
+  },
+  computed: {
+    avatarSize() {
+      switch (this.$vuetify.breakpoint.name) {
+        case "xs":
+          return "150";
+        case "sm":
+          return "155";
+        case "md":
+          return "160";
+        case "lg":
+          return "160";
+        case "xl":
+          return "160";
+        default:
+          return "160";
+      }
+    },
+    personDivSize() {
+      if (this.subheading && this.$vuetify.breakpoint.name !== "xs") return 220;
+      if (this.subheading && this.$vuetify.breakpoint.name === "xs") return 195;
+      if (!this.subheading && this.$vuetify.breakpoint.name === "xs")
+        return 180;
+      return 200;
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+@import "./src/scss/media-queries";
+
+.person-div {
+  text-align: center;
+  width: 180px;
+}
+.person-name {
+  font-size: 1em;
+  font-weight: bold;
+  margin: 10px 0px 5px 0px;
+}
+.person-character {
+  font-size: 0.85em;
+  color: darkgrey;
+}
+/* MEDIA QUERIES */
+/* SM */
+@include tablet-landscape-min {
+  //@media (max-width: 959px) {
+  .person-div {
+    width: 165px;
+  }
+  .person-name {
+    font-size: 0.9em;
+  }
+  .person-character {
+    font-size: 0.8em;
+  }
+}
+/* XS */
+@include mobile-only {
+  .person-name {
+    font-size: 0.8em;
+  }
+  .person-character {
+    font-size: 0.75em;
+  }
+}
+</style>
